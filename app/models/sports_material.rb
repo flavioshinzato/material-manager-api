f3ba78c3bd457d42c12bf2807cf50da838fc43c7
Flávio Augusto <flavioaugusto@jera.com.br>
2021-08-31 00:31:57 -0400

    Using rails admin for admin controller

--- app/models/sports_material.rb
+++ app/models/sports_material.rb
@@ -1,4 +1,5 @@
 class SportsMaterial < ApplicationRecord
+  include RailsAdmin::SportsMaterial
 
   scope :search, -> (value) { where('sports_materials.name LIKE :param OR sports_materials.description LIKE :param', param: "%#{value}%") }
 
